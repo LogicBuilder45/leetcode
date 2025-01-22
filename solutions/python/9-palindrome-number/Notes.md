c6122a9f8c01c881defba8901b16070c6aa768ff
Saqib Khan <ksaqib2010@gmail.com>
2025-01-22 14:47:24 -0600

    Delete solutions/python/9-palindrome-number directory

--- solutions/python/9-palindrome-number/Notes.md
+++
@@ -1,1 +0,0 @@
-<h2>palindrome-number Notes</h2><hr>[ Time taken: 8 m 22 s ]
--- solutions/python/9-palindrome-number/palindrome-number.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        if str(x)[::-1] == str(x):
-            return(bool(1))
-        else: return(bool(0))   
